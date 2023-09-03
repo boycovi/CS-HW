017b925f6caddd8c2ab3a7f4b1eaaea82305eb52
egrRusse3q <egorarussavskiy@gmail.com>
2023-09-03 20:56:51 +0300

    main

--- Lesson_2/Reverse String/Program.cs
+++ Lesson_2/Reverse String/Program.cs
@@ -1,3 +1,11 @@
-﻿string num = Console.ReadLine();
-Console.WriteLine(string.Concat(num.Reverse()));
+﻿Console.Write("Enter a number to reverse: ");
+int num = int.Parse(Console.ReadLine());
+int rNum = 0, r;
+while (num > 0)
+{
+    r = num % 10;
+    rNum = (rNum * 10) + r;
+    num /= 10;
+}
+Console.WriteLine(rNum);
 Console.ReadKey();
