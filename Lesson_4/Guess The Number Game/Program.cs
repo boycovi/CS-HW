a06aa587447d29d4f641a247254a25d6e6fff773
egrRusse3q <egorarussavskiy@gmail.com>
2023-09-11 13:08:44 +0300

    main

---
+++ Lesson_4/Guess The Number Game/Program.cs
@@ -0,0 +1,18 @@
+﻿bool guessed = false;
+Random random = new Random();
+byte num = (byte)random.Next(1, 146);
+
+while (!guessed)
+{
+    Console.WriteLine("Guess the number between 1 and 146");
+    var x = Console.ReadLine();
+    int.TryParse(x, out int guess);
+    if (guess > num) Console.WriteLine("Less");
+    if (guess < num) Console.WriteLine("Greater");
+    else
+    {
+        Console.WriteLine("You guessed it!");
+        guessed = true;
+    }
+}
+Environment.Exit(0);
---
+++ Lesson_4/Password Check/Program.cs
@@ -0,0 +1,16 @@
+﻿string password = "root";
+bool access = false;
+
+while (!access)
+{
+    Console.WriteLine("Enter a password: ");
+    string tryPassword = Console.ReadLine();
+    if (tryPassword == password)
+    {
+        Console.WriteLine("Access Granted");
+        Thread.Sleep(1000);
+        access = true;
+        Environment.Exit(0);
+    }
+    else Console.WriteLine("Wrong Password");
+}
