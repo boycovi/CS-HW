de6122a0c1965db64e10ee8baeb831615a98cdc5
egrRusse3q <egorarussavskiy@gmail.com>
2023-09-18 17:55:27 +0300

    main

---
+++ Lesson_6/Multiply Greater Input/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+foreach (int i in array)
+    Console.Write(i + " ");
+int counter = 1;
+Console.WriteLine("\nEnter a number: ");
+
+if (int.TryParse(Console.ReadLine(), out int num))
+{
+    foreach (int i in array)
+    {
+        if (i > num)
+            counter *= i;
+    }
+}
+else
+{
+    Console.WriteLine("Wrong Input");
+    Environment.Exit(0);
+
+}
+Console.WriteLine(counter);
---
+++ Lesson_6/Sum Even Nums/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+foreach (int i in array)
+    Console.Write(i+" ");
+int evenSum = 0;
+foreach (int n in array)
+{
+    if (n % 2 == 0)
+        evenSum += n;
+}
+Console.WriteLine("\n"+evenSum);
+Console.ReadKey();
