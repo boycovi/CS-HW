01bb109a15c1c88afc6c289430d0b8d8ec76cca1
egrRusse3q <egorarussavskiy@gmail.com>
2023-10-02 19:46:23 +0300

    main

---
+++ Lesson_9/Rectangle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Rect
+{
+    public class Rectangle
+    {
+        private double side1;
+        private double side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5.0, 3.0);
+            double area = rectangle.AreaCalculator();
+            double perimeter = rectangle.PerimeterCalculator();
+
+            Console.WriteLine("Rectangle Area: " + area);
+            Console.WriteLine("Rectangle Perimeter: " + perimeter);
+        }
+    }
+}
---
+++ Lesson_9/User/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Usr
+{
+    public class User
+    {
+        public string Login { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+        public DateTime RegistrationDate { get; }
+
+        public User(string login, string firstName, string lastName, int age, DateTime regDate)
+        {
+            Login = login;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            RegistrationDate = regDate;
+        }
+        public void GetUserInfo()
+        {
+            Console.WriteLine($"Login: {Login}\nFirstName: {FirstName}\nLastName: {LastName}\nAge: {Age}\nRegistration Date: {RegistrationDate}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("johnnnny", "John", "Doe", 99, DateTime.Now);
+            user.GetUserInfo();
+            Console.ReadKey();
+        }
+    }
+}
