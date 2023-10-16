94c7cd6fb88c027b87476c699bde8b8d94c49d30
egrRusse3q <egorarussavskiy@gmail.com>
2023-10-16 19:36:39 +0300

    main

--- Lesson_13/CarMove/Program.cs
+++ Lesson_13/CarMove/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace CarMove
+namespace CarMove
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            Console.WriteLine($"X: {myCar.Position.X}, Y: {myCar.Position.Y}");
 
+            car.Move(10, 20);
+            Console.WriteLine($"X: {myCar.Position.X}, Y: {myCar.Position.Y}");
+
+            car.StartEngine();
+            Console.WriteLine($"X: {myCar.Position.X}, Y: {myCar.Position.Y}");
+
+            car.StopEngine();
         }
     }
 }
---
+++ Lesson_13/MathUtility/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Metadata;
+
+namespace MathUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int add = MathUtility.Addition(5, 5);
+                int sub = MathUtility.Subtraction(20, 10);
+                int mult = MathUtility.Multiplication(5, 2);
+                double div = MathUtility.Division(20, 2);
+                Console.WriteLine($"{add}\n{sub}\n{mult}\n{div}");
+            }
+            catch (DivideByZeroException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadKey();
+        }
+    }
+    public static class MathUtility
+    {
+        public static int Addition(int a, int b) { return a + b; }
+        public static int Subtraction(int a, int b) { return a - b; }
+        public static int Multiplication(int a, int b) { return a * b; }
+        public static double Division(int a, int b)
+        {
+            if (a == 0)
+                throw new DivideByZeroException("Division by zero");
+
+            return (double)(a / b);
+        }
+    }
+}
