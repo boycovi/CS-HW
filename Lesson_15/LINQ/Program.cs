a584831ecd350fadcd9cb3cc3a49ff725e1646d7
egrRusse3q <egorarussavskiy@gmail.com>
2023-10-23 18:33:04 +0300

    main

---
+++ Lesson_15/LINQ/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Filtering
+            List<int> intList = new List<int> { 2, 5, 8, 12, 15, 18, 22 };
+            intList = intList.Where(p => p > 10).ToList();
+            foreach (int i in intList)
+            {
+                Console.Write(i + " \n");
+            }
+
+            //2.Sorting
+            List<string> fruitsList = new List<string> { "Orange", "Kiwi", "Apple", "Banana" };
+            fruitsList.Sort();
+            Console.WriteLine("");
+            foreach (string i in fruitsList)
+            {
+                Console.Write(i + " \n");
+            }
+
+            //3.Delete duplicates
+            List<string> charList = new List<string> { "a", "b", "c", "a", "d", "c" };
+            var uniqueStrings = charList.Distinct();
+            Console.WriteLine();
+            foreach (var i in uniqueStrings)
+            {
+                Console.WriteLine(i);
+            }
+
+            //4.Count
+            List<int> grades = new List<int> { 85, 92, 78, 95, 88, 90 };
+            Console.WriteLine();
+            Console.WriteLine(grades.Count(p=>p>90));
+
+            //5.Filtering+Sorting
+            List<Product> products = new List<Product>
+        {
+            new Product { Name = "Prod1", Price = 45 },
+            new Product { Name = "Prod2", Price = 60 },
+            new Product { Name = "Prod3", Price = 30 },
+            new Product { Name = "Prod4", Price = 55 },
+        };
+            var prodFiltered = products.Where(products => products.Price < 50).OrderBy(products=>products.Price).ToList();
+            Console.WriteLine();
+            foreach (var i in prodFiltered)
+            {
+                Console.WriteLine($"{i.Name}:{i.Price}$");
+            }
+
+            //6.Find max by index
+            List<int> nums = new List<int> { 10, 25, 8, 45, 15, 30, 55, 5 };
+            Console.WriteLine(nums.IndexOf(nums.Max()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Students/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+            new Student { Name = "Olena", Grade = 95, Course = 1 },
+            new Student { Name = "Petro", Grade = 88, Course = 2 },
+            new Student { Name = "Timofy", Grade = 94, Course = 2 },
+            new Student { Name = "Anna", Grade = 92, Course = 1 },
+            new Student { Name = "Mykita", Grade = 78, Course = 3 },
+            new Student { Name = "Irina", Grade = 90, Course = 2 },
+            new Student { Name = "Andriy", Grade = 91, Course = 3 },
+            };
+            //1
+            List<Student> highestScore = students.Where(p => p.Grade >= 90).ToList();
+            Console.WriteLine("Students with highest scores:\n");
+            foreach (Student student in highestScore)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine();
+
+            //2
+            List<Student> descendingOrder = students.OrderByDescending(p => p.Grade).ToList();
+            Console.WriteLine("Descending order of student by grade:\n");
+            foreach (Student student in descendingOrder)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine();
+
+            //3
+            List<Student> firstCourse = students.Where(p => p.Course == 1).ToList();
+            List<Student> secondCourse = students.Where(p => p.Course == 2).ToList();
+            List<Student> thirdCourse = students.Where(p => p.Course == 3).ToList();
+            Console.WriteLine("Student of the first course:\n");
+            foreach (Student student in firstCourse)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Student of the second course:\n");
+            foreach (Student student in secondCourse)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Student of the third course:\n");
+            foreach (Student student in thirdCourse)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine();
+
+            //4
+            int firstCourseStudentCount = students.Count(p => p.Course == 1);
+            int secondCourseStudentCount = students.Count(p => p.Course == 2);
+            int thirdCourseStudentCount = students.Count(p => p.Course == 3);
+            Console.WriteLine($"Total students at first course:{firstCourseStudentCount}");
+            Console.WriteLine($"Total students at second course:{secondCourseStudentCount}");
+            Console.WriteLine($"Total students at third course:{thirdCourseStudentCount}");
+            Console.WriteLine();
+
+            //5
+            var studentInfo = students.Select(student => $"Name: {student.Name}, Grade: {student.Grade}");
+            foreach (var info in studentInfo)
+            {
+                Console.WriteLine(info);
+            }
+
+        }
+    }
+}
